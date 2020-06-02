46fe70be7c902bf39b41587595609f003fd50b50
bimalmagar10 <bimalmagar873@gmail.com>
2020-06-02 20:58:54 +0545

    change protocol https to http

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
  data() {
   return {
       api_key:'a003634552d8204b115d0beb72c8e7a7',
-      url_base:'http://api.openweathermap.org/data/2.5/',
+      url_base:'https://api.openweathermap.org/data/2.5/',
       query:'',
       weather:{}
 
